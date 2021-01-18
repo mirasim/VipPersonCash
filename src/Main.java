9ff4fb92cd38d47d6df3466cd1c51c07c88839e5
Расим <yamirasim@yandex.ru>
2021-01-19 01:16:11 +0300

    klhuj,lh

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int currentBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int result = 0;
+
+        result = currentBalance + transferAmount;
+       System.out.println ("ShowResult=" + result);
+
+
     }
 }
